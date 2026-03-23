8dbd8f643fb01e456966557a93413b116dcf98d9
Benjamin Yde <benjamin.yde@vintecc.com>
2026-03-23 19:38:58 +0000

    feat(docker): devcontainer: added python sub generation + fix(docker): fix issue where python reloaded random in the terminal

--- .devcontainer/python_ubuntu24_x64/boostrap_container.sh
+++ .devcontainer/python_ubuntu24_x64/boostrap_container.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR=$(dirname $0)
-cd $CURRENT_DIR
+REPO_DIR=$(realpath $CURRENT_DIR/../..)
 
 # configure shell rc's:
+cd $CURRENT_DIR
 for shellrc in .shrc .bashrc .zshrc; do
     cat shellrc >> ~/$shellrc
 done
+
+# source shellrc in current shell session
+source ./shellrc
+
+# python: generate stubs
+cd $REPO_DIR
+./python/tools/stubs/generate_usd_stubs.sh
